b4114980c79172a13fa32ab1d7bfde239c4c57e6
Андрей Боярко <andreyboyarko@gmail.com>
2025-01-19 16:06:31 +0100

    Дороботка, исправление ошибок

--- MovieQuiz/Services/QuestionFactoryProtocol.swift
+++ MovieQuiz/Services/QuestionFactoryProtocol.swift
@@ -1,8 +1,4 @@
-//import Foundation
-//
-//protocol QuestionFactoryProtocol {
-//    func requestNextQuestion()
-//}
+
 protocol QuestionFactoryProtocol {
     func requestNextQuestion() -> QuizQuestion?
     func setup(delegate: QuestionFactoryDelegate)
